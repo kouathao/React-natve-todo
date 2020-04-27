587f472043e79fdf4fc5ade23d804a361e34723a
Koua Thao <kouathao09@gmail.com>
2020-04-26 23:45:59 -0500

    Completed simple todo app

---
+++ App.js
@@ -0,0 +1,84 @@
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  Alert,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from "react-native";
+
+// components
+import Header from "./components/Header";
+import TodoItem from "./components/TodoItem";
+import AddTodo from "./components/AddTodo";
+import Sandbox from "./components/Sandbox";
+
+export default function App() {
+  const [todos, setTodos] = useState([
+    { text: "but coffee", key: "1" },
+    { text: "create an app", key: "2" },
+    { text: "play on the switch", key: "3" },
+  ]);
+
+  //on press for todoitem and pass down to todoitem component
+  const pressHandler = (key) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter((todo) => todo.key != key);
+    });
+  };
+
+  // addtodo function to pass down as props to Addtodo component
+  const submitHandler = (text) => {
+    // validation
+    if (text.length > 2) {
+      setTodos((prevTodos) => {
+        return [{ text: text, key: Math.random().toString() }, ...prevTodos];
+      });
+    } else {
+      Alert.alert("OPPS!", "Todos must be over 2 character log", [
+        { text: "Understood", onPress: () => console.log("alert closed") },
+      ]);
+    }
+  };
+
+  return (
+    // <Sandbox />
+    <TouchableWithoutFeedback
+      onPress={() => {
+        Keyboard.dismiss();
+      }}
+    >
+      <View style={styles.container}>
+        <Header />
+        <View style={styles.content}>
+          <AddTodo submitHandler={submitHandler} />
+          <View style={styles.list}>
+            <FlatList
+              data={todos}
+              renderItem={({ item }) => (
+                <TodoItem item={item} pressHandler={pressHandler} />
+              )}
+            />
+          </View>
+        </View>
+      </View>
+    </TouchableWithoutFeedback>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+  content: {
+    padding: 40,
+    flex: 1,
+  },
+  list: {
+    flex: 1,
+    marginTop: 20,
+  },
+});
